57ecb9da6c579503d9788437ba54ba6551653f51
mcdanije22 <mcdanije22@gmail.com>
2019-09-04 23:55:48 -0400

    updating ui files

--- client/src/components/foodCard/FoodCard.js
+++ client/src/components/foodCard/FoodCard.js
@@ -3,18 +3,8 @@ import './foodCard.scss';
 
 const FoodCard = () =>{
     return(
-        <div className = 'foodCardContainer'>
-                <img src = 'https://via.placeholder.com/170x120' />
-                <div className = 'bottomCard'>
-                    <h3>Keto Pizza</h3> 
-                    <p>By: Josh McDaniel</p>
-                    <p>20p 20f 50c </p>
-                    <div className = 'bottomTags'>
-                        <button>Keto</button>
-                        <button>Protein</button>
-                    </div>
-                </div>
+        <div className= 'FoodCard'>
+
         </div>
     )
-};
-export default FoodCard;
+}
--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './home.scss';
-import FoodCard from '../foodCard/FoodCard';
 
 const Home = () =>{
     return(
         <div id = 'home'>
         <div id = "cards">
-        <FoodCard />
-        <FoodCard />
+       
         </div>
             {/* <div id = 'topImg'>
                 <img src = 'https://via.placeholder.com/400x250' />
