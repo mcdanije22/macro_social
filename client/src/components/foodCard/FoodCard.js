6600c3a2d0925a678143aeec23221a638467c0c3
mcdanije22 <mcdanije22@gmail.com>
2019-09-05 21:01:07 -0400

    food card ui

--- client/src/components/foodCard/FoodCard.js
+++ client/src/components/foodCard/FoodCard.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import './foodCard.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart, faPaperPlane, faComment } from '@fortawesome/free-regular-svg-icons';
+
 
 const FoodCard = () =>{
     return(
-        <div className= 'FoodCard'>
-
+        <div className='foodCard'>
+            <div className='cardTopRow'>
+                <img src='https://via.placeholder.com/50' />
+                <h3>Keto Pizza</h3>
+            </div>
+            <hr />
+            <div className='cardImage'>
+                <img src='https://via.placeholder.com/300x200' />
+            </div>
+            {/* <hr /> */}
+            <div className='bottomCard'>
+                <div className='bottomCardRowone'>
+                    <h1>20P 2C 9F</h1> 
+                    <p>200 Saves</p>
+                </div>
+                <div className='bottomCardRowtwo'>
+               <button type='button'><FontAwesomeIcon className='saveIcon'  icon={faHeart} /></button>     
+               <button type='button'><FontAwesomeIcon className='saveIcon'  icon={faPaperPlane} /></button> 
+               <button type='button'><FontAwesomeIcon className='saveIcon'  icon={faComment} /></button>  
+                  
+                </div>
+            </div> 
+            <div className='bottomCardFooter'>
+                    <button>Keto</button>
+                    <button>Protien</button>
+                </div>
         </div>
     )
 }
+export default FoodCard;
--- client/src/components/footerMenu/FooterMenu.js
+++ client/src/components/footerMenu/FooterMenu.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './footerMenu.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus, faHome } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faBell, faUser } from '@fortawesome/free-regular-svg-icons';
 
 const FooterMenu = () =>{
     return(
         <div id = 'footerMenu'>
             <ul id = 'buttonGroup'>
-                <li>T</li>
-                <li>T</li>
-                <li>T</li>
-                <li>T</li>
-                <li>T</li>
-                <li>T</li>
+                <button type='button'><FontAwesomeIcon className='footerMenuButtons' icon={faHome} /></button>     
+                <button type='button'><FontAwesomeIcon className='footerMenuButtons' icon={faHeart} /></button>     
+                <button type='button'><FontAwesomeIcon className='footerMenuButtons' icon={faPlus} /></button>     
+                <button type='button'><FontAwesomeIcon className='footerMenuButtons' icon={faBell} /></button>     
+                <button type='button'><FontAwesomeIcon className='footerMenuButtons' icon={faUser} /></button>     
             </ul>
         </div>
     )
